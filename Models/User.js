a677ddde2fe333389f1d4909ec775420fe31ccf1
miok-jung <miok.dev@gmail.com>
2022-05-11 12:01:03 +0900

    feat: bcrypt - bcrypt로 회원가입시 비밀번호 암호화하기

--- Models/User.js
+++ Models/User.js
@@ -22,12 +22,20 @@ const userSchema = mongoose.Schema(
   { collection: 'users' },
 );
 
-// 특정 동작 이전에 실행으로 save를 하기 전에 호출이 된다.
-// 도큐먼트 저장전 최종 검증으로 사용할 수도 있다.
-// userSchema.pre('save', (next) => {
-//   let user = this; //??
-//   if(user.isModified('password')){}
-// })
+userSchema.pre('save', (next) => {
+  var user = this;
+
+  if (user.isModified('password')) {
+    bcrypt.genSalt(saltRounds, (err, salt) => {
+      if (err) return next(err);
+      bcrypt.hash(user.password, salt, (err, hash) => {
+        if (err) return next(err);
+        user.password = hash;
+        next();
+      });
+    });
+  }
+});
 
 const User = mongoose.model('User', userSchema);
 
