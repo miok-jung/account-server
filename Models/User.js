562fec506ad7a38efa5d1af62fa1ea16c06ac44c
miok-jung <miok.dev@gmail.com>
2022-05-13 13:45:29 +0900

    feat: Login - 로그인 해쉬 검증하기

--- Models/User.js
+++ Models/User.js
@@ -22,20 +22,29 @@ const userSchema = mongoose.Schema(
   { collection: 'users' },
 );
 
-userSchema.pre('save', (next) => {
+userSchema.pre('save', function (next) {
   var user = this;
 
   if (user.isModified('password')) {
-    bcrypt.genSalt(saltRounds, (err, salt) => {
+    bcrypt.genSalt(saltRounds, function (err, salt) {
       if (err) return next(err);
-      bcrypt.hash(user.password, salt, (err, hash) => {
+      bcrypt.hash(user.password, salt, function (err, hash) {
         if (err) return next(err);
         user.password = hash;
         next();
       });
     });
+  } else {
+    next();
   }
 });
+
+userSchema.methods.comparePassword = function (plainPassword, cb) {
+  bcrypt.compare(plainPassword, this.password, (err, isMatch) => {
+    if (err) return cb(err);
+    cb(null, isMatch);
+  });
+};
 
 const User = mongoose.model('User', userSchema);
 
--- Router/user.js
+++ Router/user.js
@@ -7,10 +7,44 @@ const { User } = require('../Models/User');
 router.post('/register', (req, res) => {
   // 인스턴스 생성 : 할당된 실체
   const user = new User(req.body); // body-parser을 통해 json 형태를 가져올 수 있다.
-  user.save((err, userInfo) => {
-    if (err) return res.json({ success: false, err });
-    return res.status(200).json({ success: true });
-  });
+
+  user
+    .save()
+    .then(() => {
+      res.status(200).json({ success: true });
+    })
+    .catch((err) => {
+      return res.json({ success: false, err });
+    });
+  // TODO 같은 내용???
+  // user.save((err, userInfo) => {
+  //   if (err) return res.json({ success: false, err });
+  //   return res.status(200).json({ success: true });
+  // });
 });
 
+router.post('/login', (req, res) => {
+  // 1. 요청된 이메일을 데이터베이스에서 있는지 찾기
+  User.findOne({ email: req.body.email }, (err, user) => {
+    if (!user) {
+      return res.json({
+        loginSuccess: false,
+        message: '제공된 이메일에 해당되는 유저가 없습니다.',
+      });
+    }
+    // 2. DB에서 요청한 이메일이 있는 경우, 비밀번호가 맞는 비밀번호인지 확인하기
+    user.comparePassword(req.body.password, (err, isMatch) => {
+      if (!isMatch)
+        return res.status(401).json({
+          loginSuccess: false,
+          message: '비밀번호가 틀렸습니다.',
+        });
+
+      // 3. 비밀번호까지 같다면 토큰을 생성하기
+      // user.generateToken((err, user) => {
+      //   // ...
+      // });
+    });
+  });
+});
 module.exports = router;
